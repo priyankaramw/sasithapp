47a7c5e52c0e2cad15864fa677e83caaf6094c95
Sasith Priyankara <priyankaramw@gmail.com>
2019-03-10 23:14:33 +0800

    A

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -19,5 +19,19 @@ namespace Vidly.Controllers
         public ActionResult Edit(int id, string name) {
             return Content("Id = "+id+ ", Name = "+name);
         }
+
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("PageIndex = {0}, SortBy = {1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month) {
+            return Content(String.Format("{0}/{1}", year, month));
+        }
     }
 }
